cbd59c81d724dd05adc66d2a6ecb4620767626a0
Alexandre <alevaristofig@gmail.com>
2026-02-22 17:11:25 -0300

    Implementação da pagina index carteira

--- src/paginas/carteira/index.tsx
+++ src/paginas/carteira/index.tsx
@@ -1,11 +1,87 @@
 import { ReactElement, useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { RootState } from "../../redux/root-reducer";
+import { buscar } from "../../redux/carteira/slice";
+
+import Table from 'react-bootstrap/Table';
+
+import { PiNotePencilLight } from "react-icons/pi";
+
+import Cabecalho from "../../components/cabecalho";
+import Menu from "../../components/menu";
 
 const Carteira = (): ReactElement => {
+
+    const dispatch = useDispatch();
+    const { loading, carteiras } = useSelector((state: RootState) => state.carteira);
+    const navigate = useNavigate();
+
+    const IconeEditar = PiNotePencilLight as unknown as React.FC<React.SVGProps<SVGSVGElement>>;
+
+    const formatarData = (data: string) => {
+        const dataFormatada = new Date(data);
+
+        const dataExibicao = dataFormatada.toLocaleString("pt-BR");
+
+        return dataExibicao.replace(',','');
+    }
+
     return(
         <>
-            <div>Index carteira</div>
+            <Cabecalho />
+            <div className='d-flex mt-3'>
+                <Menu />
+                 <div className="container-fluid">
+                    {
+                        loading
+                        ?
+                            <div className="spinner-border text-primary mt-3" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        :
+                             carteiras.length === 0
+                             ?
+                                <div className='me-2 float-start w-100'>
+                                    Não existem dados para exibir
+                                </div>
+                             :
+                                <Table className="responsive striped bordered hover">
+                                    <thead>
+                                        <tr>
+                                            <th scope='col'>Carteira Id</th>                        
+                                            <th scope='col'>Descrição</th>
+                                            <th scope='col'>Valor</th> 
+                                            <th scope='col'>Status</th>                                               
+                                            <th scope='col'>Data</th>                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            carteiras.map((p: any,i: number) => 
+                                            (
+                                                    <tr key={p['id']}>                                                        
+                                                        <td>{p['carteria_id']}</td>
+                                                        <td>{p['descricao']}</td>
+                                                        <td>{p['valor'].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td> 
+                                                        <td>{p['status']}</td>                                                        
+                                                        <td>{formatarData(p['created_at'])}</td>   
+                                                        <td>
+                                                            <Link to={`/editarcarteira/${p['id']}`} 
+                                                                className="btn btn-info float-start me-1 text-white"
+                                                                title='Editar'>
+                                                                <IconeEditar />        
+                                                            </Link>
+                                                        </td>                                                                        
+                                                    </tr>
+                                                ))
+                                        }
+                                    </tbody>
+                                </Table>
+                    }
+                 </div>
+            </div>
         </>
     )
 }
