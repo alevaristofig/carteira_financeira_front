48eca15b4ab2f7d0bc5acd8da99ff3185be8d69c
Alexandre <alevaristofig@gmail.com>
2026-02-22 16:50:04 -0300

    Criação das rotas carteira e cadastro carteira

---
+++ src/paginas/carteira/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement, useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+
+const Carteira = (): ReactElement => {
+    return(
+        <>
+            <div>Index carteira</div>
+        </>
+    )
+}
+
+export default Carteira; 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import CadastroUsuario from "./paginas/Usuario/CadastroUsuario";
+import Carteira from "./paginas/carteira";
+import CadastroCarteira from "./paginas/carteira/CadastroCarteira";
 
 const router = createBrowserRouter([
     {
         path: '/cadastrousuario',
         element: <CadastroUsuario />
+    },
+    {
+        path: '/carteira',
+        element: <Carteira />
+    },
+    {
+        path: '/cadastrocarteira',
+        element: <CadastroCarteira />
     }
 ]);
 
