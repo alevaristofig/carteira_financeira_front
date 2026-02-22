bcebe0ca3f0c2457871a5d10dcfd98d2820172e6
Alexandre <alevaristofig@gmail.com>
2026-02-22 12:07:47 -0300

    Implementação da pagina cadastroUsuario

--- src/paginas/Usuario/CadastroUsuario.tsx
+++ src/paginas/Usuario/CadastroUsuario.tsx
@@ -1,12 +1,91 @@
-import { ReactElement, useState, FormEvent, useEffect } from "react";
+import { ReactElement, useState, SubmitEvent , useEffect } from "react";
+
+import Row  from 'react-bootstrap/Row';
+import Col  from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Form  from 'react-bootstrap/Form';
+import Button  from 'react-bootstrap/Button';
 
 import Cabecalho from "../../components/cabecalho";
+import Menu from "../../components/menu";
 
 const CadastroUsuario = (): ReactElement => {
+
+    const [nome,setNome] = useState<string>('');
+    const [email,setEmail] = useState<string>('');
+    const [senha,setSenha] = useState<string>('');
+
+    const cadastrar = (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        let dados = {
+            'name': nome,
+            'email': email,
+            'password': senha
+        }
+    }
+
     return(
         <>
             <Cabecalho />
-            <div>Cadastro Usuario</div>
+            <div className='d-flex mt-3'>
+                <Menu />
+                <div className="container-fluid">
+                    <Form onSubmit={cadastrar}>
+                        <Card>
+                            <Card.Body>
+                                <Form.Group className='mb-4'>
+                                    <Row className="mb-4">
+                                        <Col xs={1}>
+                                             <Form.Label>Nome*:</Form.Label>                                             
+                                        </Col>
+                                        <Col xs={10}>
+                                            <Form.Control 
+                                                type='text' 
+                                                onChange={(e) => setNome(e.target.value)}
+                                                value={nome}
+                                                required
+                                            >
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-4">
+                                        <Col xs={1}>
+                                             <Form.Label>Email*:</Form.Label>                                             
+                                        </Col>
+                                        <Col xs={10}>
+                                            <Form.Control 
+                                                type='text' 
+                                                onChange={(e) => setEmail(e.target.value)}
+                                                value={nome}
+                                                required
+                                            >
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                    <Row className="mb-4">
+                                        <Col xs={1}>
+                                             <Form.Label>Senha*:</Form.Label>                                             
+                                        </Col>
+                                        <Col xs={10}>
+                                            <Form.Control 
+                                                type='password' 
+                                                onChange={(e) => setSenha(e.target.value)}
+                                                value={nome}
+                                                required
+                                            >
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                                <Form.Group className='mt-4'>
+                                    <Button type='submit'>Salvar</Button>
+                                </Form.Group> 
+                            </Card.Body>
+                        </Card>
+                    </Form>
+                </div>
+            </div>
         </>
     )
 }
