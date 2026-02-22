5f81478b378b12f3a88d938eaf8b6ef94e0860e2
Alexandre <alevaristofig@gmail.com>
2026-02-22 15:23:51 -0300

    Implementação da função buscar no redux saga operacao

--- src/redux/operacao/saga.tsx
+++ src/redux/operacao/saga.tsx
@@ -1,7 +1,7 @@
 import { all, takeEvery, put, call } from "redux-saga/effects";
 import { AnyAction } from "redux-saga";
 
-import { listarSucesso, listarError } from "./slice";
+import { listarSucesso, listarError, buscarSucesso, buscarError } from "./slice";
 
 import axios, { AxiosResponse } from 'axios';
 import { IOperacao } from "../../interfaces/operacao.interface";
@@ -20,8 +20,22 @@ function* listar(action: AnyAction): Generator<any, void, AxiosResponse<IOperaca
   }
 }
 
+function* buscar(action: AnyAction): Generator<any, void, AxiosResponse<IOperacao>>  {
+    try {
+        const response = yield call(axios.get,`http://localhost:8000/api/carteira_financeira/operacao/${action.id}`,{
+            /*headers: {
+                "Authorization": `Bearer ${sessionStorage.getItem('token')}`
+            }*/
+        });
+
+        yield put(listarSucesso(response.data));
+  } catch(error: any) {    
+     yield put(listarError(error.response.data.message));
+  }
+}
+
 export default all([
-   // takeEvery('carteira/salvar', salvar),
-   // takeEvery('carteira/buscar', buscar),
+    takeEvery('operacao/listar', listar),
+    takeEvery('operacao/buscar', buscar),
   //  takeEvery('carteira/atualizar', atualizar),
 ]);
