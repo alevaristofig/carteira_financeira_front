9b8c5541799c22b1f5a2215f9bc12edbd437924d
Alexandre <alevaristofig@gmail.com>
2026-02-22 12:23:36 -0300

    Implementação da interface usuario

--- src/redux/usuario/saga.tsx
+++ src/redux/usuario/saga.tsx
@@ -1,7 +1,12 @@
 import { all, takeEvery, put, call } from "redux-saga/effects";
 import { AnyAction } from "redux-saga";
 
+import { salvarSucesso, salvarError } from "./slice";
+
 import axios, { AxiosResponse } from 'axios';
+import { IUsuario } from "../../interfaces/usuario.interface";
+
+function* salvar(action: AnyAction): Generator<any, void, AxiosResponse<IUsuario>>  {}
 
 
 export default all([
