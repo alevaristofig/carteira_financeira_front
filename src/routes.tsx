facc845007206320168d2ef06754fdd1ad2d925c
Alexandre <alevaristofig@gmail.com>
2026-02-22 17:41:46 -0300

    Implementação da pagina cadastro transferencia e criação da sua rota

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import CadastroCarteira from "./paginas/carteira/CadastroCarteira";
 import EditarCarteira from "./paginas/carteira/EditarCarteira";
 import Operacao from "./paginas/operacao";
 import CadastroDeposito from "./paginas/operacao/CadastroDeposito";
+import CadastroTransferencia from "./paginas/operacao/CadastroTransferencia";
 
 const router = createBrowserRouter([
     {
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
     {
         path: '/operacao/deposito',
         element: <CadastroDeposito />
+    },
+    {
+        path: '/operacao/transferencia',
+        element: <CadastroTransferencia />
     },
 ]);
 
